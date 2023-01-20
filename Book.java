38a226829b677f9daca61a576c35781fb99f0db6
Alper Karabıyık <120407040+karabiyikk@users.noreply.github.com>
2023-01-20 12:44:08 +0300

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,54 @@
+package KitapSiralayici;
+
+import java.util.Comparator;
+
+public class Book implements Comparator<Book> {
+    private String name;
+    private int pageNumber;
+    private String authorName;
+    private int releaseDate;
+
+    public Book(String name, int pageNumber, String authorName, int releaseDate) {
+        this.name = name;
+        this.pageNumber = pageNumber;
+        this.authorName = authorName;
+        this.releaseDate = releaseDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPageNumber() {
+        return pageNumber;
+    }
+
+    public void setPageNumber(int pageNumber) {
+        this.pageNumber = pageNumber;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public int getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(int releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    @Override
+    public int compare(Book o1, Book o2) {
+        return o1.getName().compareTo(o2.getName());
+    }
+}
